0e20d6fe46c5096ef005571755cacb4289fe0a77
V1tale <elativ.c@gmail.com>
2022-10-20 15:49:03 +0200

    completato

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-
-// Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
-
-
 const people = [ 
     { 
      name: "Wayne Barnett",
@@ -42,5 +38,5 @@ let box = document.getElementById("wrapper");
 for ( i = 0; i < people.length; i++) {
     let thisPerson = people[i];
     console.log(thisPerson.name, thisPerson.role, thisPerson.picture);
-    box.innerHTML += `<h2>${thisPerson.name}</h2><h3>${thisPerson.role}</h3><img src="img/${thisPerson.picture}">`;
+    box.innerHTML += `<div class="card"><img src="img/${thisPerson.picture}"><div class="text"><h2>${thisPerson.name}</h2><h2>${thisPerson.role}</h2></div></div>`;
 }
